ffe7d73f414cc337ff29dbd29795861b5ccb29e5
kateosadchaya <63636450+kateosadchaya@users.noreply.github.com>
2020-04-20 23:00:44 +0300

    Create Lab3,task1.js

---
+++ Lab3,task1.js
@@ -0,0 +1,8 @@
+function game(){
+	let partner = prompt('Яке імя у вашого партнера?');
+    let child = prompt('Скільки ви хочете дітей?');
+    let city = prompt('В якому місті ви хочете жити?');
+    let career = prompt('Яка у вас професія?');
+
+    alert(`"Ви укладете шлюб з ${partner} та у вас буде ${child} дітей" . "Ви переїдете у місто ${city} на посаду ${career} " `)
+}
